3a671a7c207a2a2508df816157b0561a39cdc6ca
Lợi Nguyễn <loinguyen@chotot.vn>
2016-02-23 21:01:11 +0700

    11242 gets accepted

--- practice/java/uva/11242.cpp
+++ practice/java/uva/11242.cpp
@@ -9,43 +9,46 @@ using namespace std;
 
 typedef vector<int> vi;
 
-
 int main() {
 	int front, rear;
-	scanf("%d %d", &front, &rear);
-	vi frontVector(0);
-	vi rearVector(0);
-	REP(i, 0, front -1)
-	{
-		int teeth;
-		scanf("%d", &teeth);
-		frontVector.push_back(teeth);
-	}
-
-	REP(i, 0, rear -1 )
-	{
-		int teeth;
-		scanf("%d", &teeth);
-		rearVector.push_back(teeth);
-	}
-
-	vector<double> dVector(0);
-
-	REP(i, 0, front - 1) {
-		REP(j, 0, rear - 1){
-			double tmp = (double) frontVector[i]/rearVector[j];
-			dVector.push_back(tmp);
+	while (scanf("%d", &front), front) {
+		scanf("%d", &rear);
+		vi frontVector(0);
+		vi rearVector(0);
+		REP(i, 0, front -1)
+		{
+			int teeth;
+			scanf("%d", &teeth);
+			frontVector.push_back(teeth);
 		}
-	}
-	sort(dVector.begin(), dVector.end());
-	double max = 0;
-	for (std::vector<double>::iterator it = dVector.begin(); it!=dVector.end()-1; it++) {
-		double val = *(it+1)/(*it);
-		val =  roundf(val * 100) / 100;
-		if (max < val) max = val;
-	}
-	cout << max << endl;
 
+		REP(i, 0, rear -1 )
+		{
+			int teeth;
+			scanf("%d", &teeth);
+			rearVector.push_back(teeth);
+		}
+
+		vector<double> dVector(0);
+
+		REP(i, 0, front - 1)
+		{
+			REP(j, 0, rear - 1)
+			{
+				double tmp = (double) frontVector[i] / rearVector[j];
+				dVector.push_back(tmp);
+			}
+		}
+		sort(dVector.begin(), dVector.end());
+		double max = 0;
+		for (std::vector<double>::iterator it = dVector.begin();
+				it != dVector.end() - 1; it++) {
+			double val = *(it + 1) / (*it);
+			if (max < val)
+				max = val;
+		}
+		printf("%.2lf\n", max);
+	}
 
 	return 0;
 }
